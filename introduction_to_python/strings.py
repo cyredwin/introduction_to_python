1a0a48c68a108f0b25d0955b9f629e772ac4e61b
EdwinandLinux <edwin198621@outlook.com>
2026-08-01 22:44:56 -0400

    Updated strings.py file

--- introduction_to_python/strings.py
+++ introduction_to_python/strings.py
@@ -47,3 +47,16 @@ name_length = len(given_name + " " + middle_names + " " + family_name)  # calcul
 # Nothing you need to do here
 driving_license_character_limit = 28
 print(name_length <= driving_license_character_limit)
+
+
+'''
+Calculate and print the total sales for the week from the data provided. 
+Print out a string of the form "This week's total sales: xxx", where xxx will be the actual total of all the numbers. You’ll need to change the type of the input data in order to calculate that total.
+'''
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+total_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+print("This week's total sales: " + str(total_sales))
